32fe06ac72b1cd50f4119cc08956e2c0c90f337d
JT Kawaski <kawaskij@mail.gvsu.edu>
2017-09-08 17:37:25 -0400

    merged my dogs

--- dog.c
+++ dog.c
@@ -3,17 +3,4 @@ canine
 wolf
 bark
 bow wow
-<<<<<<< HEAD
-pikachu
-charmander
-squirtle
-bulbasaur
-charmelon
-charizard
-ivysaur
-venasaur
-wartortle
-blastoise
-=======
 grr
->>>>>>> 592eeafb3e2b03db5785f6917e1ca51dbabd2cf3
